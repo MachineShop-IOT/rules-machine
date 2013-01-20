d989a38a634ebeedae922c9d6a37dfae89ff520d
TimothyClayton <timothy.daniel.clayton@gmail.com>
2013-01-20 12:35:15 -0700

    lat, long, etc. dropdown now populating for device instances whose device type has payload data

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -16,22 +16,27 @@ class RulesController < ApplicationController
     @rule = get_rule(params[:id])
   end
 
-  def new
+    def new
     #call GET/device_instance endpoint
     @device_instances = get_device_instances
-    @device_ids = []
-    @device_ids_all = []
+    device_ids = []
     @device_device_instances = {}
+
     @device_instances.each do |di|
-      @device_ids_all << di[:device_id]
-      @device_device_instances[di[:device_id]] = di[:_id]
+      device_ids << di[:device_id]
+      if @device_device_instances[di[:device_id]]
+        @device_device_instances[di[:device_id]]
+        @device_device_instances[di[:device_id]] << di[:_id]
+      else
+        @device_device_instances[di[:device_id]] = [di[:_id]]
+      end
     end
-    @device_ids = @device_ids_all.uniq if @device_ids_all.length > 0
+    device_ids = device_ids.uniq if device_ids.length > 0
     @device_device_instances = @device_device_instances.to_json
 
     #call GET/device endpoint
     @devices = []
-    @device_ids.each do |id|
+    device_ids.each do |id|
       @devices << get_device(id)
     end
 
@@ -56,6 +61,47 @@ class RulesController < ApplicationController
       format.json { render json: @rule }
     end
   end
+
+  # def new
+  #   #call GET/device_instance endpoint
+  #   @device_instances = get_device_instances
+  #   @device_ids = []
+  #   @device_ids_all = []
+  #   @device_device_instances = {}
+  #   @device_instances.each do |di|
+  #     @device_ids_all << di[:device_id]
+  #     @device_device_instances[di[:device_id]] = di[:_id]
+  #   end
+  #   @device_ids = @device_ids_all.uniq if @device_ids_all.length > 0
+  #   @device_device_instances = @device_device_instances.to_json
+
+  #   #call GET/device endpoint
+  #   @devices = []
+  #   @device_ids.each do |id|
+  #     @devices << get_device(id)
+  #   end
+
+  #   #get all device payloads
+  #   @device_properties = {}
+  #   @devices.each do |device|
+  #     device[:payloads] = [] if !device[:payloads]
+  #     device_props = []
+  #     device[:payloads].each do |payload|
+  #       device_props << payload[:key_name]
+  #       end if @device_properties[device[:_id]] = device_props
+  #       @device_properties[device[:id]] = device_props
+  #   end
+  #   @device_properties = @device_properties.to_json
+
+  #   # Call endpoint to retrieve all possible rule conditions
+  #   @join_rule_conditions = get_join_rule_conditions.to_json
+  #   @comparison_rule_conditions = get_comparison_rule_conditions.to_json
+
+  #   respond_to do |format|
+  #     format.html
+  #     format.json { render json: @rule }
+  #   end
+  # end
 
   def create
     rule = create_rule(params[:rule_json])
